f14d2ceb25b343cb1d42adaef88394dd9c2549f4
royi barboy <royibarboy@gmail.com>
2023-01-28 02:20:17 +0200

    git commit

--- FinalProject/mainJS.js
+++ FinalProject/mainJS.js
@@ -1,3 +1,18 @@
-﻿function linkTest(){
-   alert("hello world") 
-}
+﻿document.addEventListener("DOMContentLoaded", function() {
+   // script 1
+   const modal = document.querySelector(".modal");
+   modal.showModal();
+ 
+   // script 2
+   const button = document.querySelector("#newsletterOpen");
+   button.addEventListener("click", function() {
+     window.open("https://news-letter-signup.onrender.com/", "_blank");
+   });
+ 
+   // script 3
+   const button2 = document.querySelector("#close-button")
+   button2.addEventListener("click", function(){
+     modal.style.display = "none"
+   })
+ });
+ 
